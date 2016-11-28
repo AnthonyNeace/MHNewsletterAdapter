aca7b3457180efcecbd81cfe1a71e6c3105a6ed3
Anthony Neace <hyperanthony@gmail.com>
2016-11-27 21:45:31 -0500

    Moved description replace regexes to config
    Moved regex literals for cleaning up the newsletter plaintext out to the
    root config file.  This will make it easier to catch and clean up subtle
    changes in the newsletter over time without having to touch the actual
    code.

--- config.js
+++ config.js
@@ -9,4 +9,22 @@ config.feedManagers = [
     require('./lib/mercyhill/feeds/alexa-serveteamfeedmanager.js')
 ];
 
+config.replaceMap = new Map();
+// Remove all text between square brackets, and the brackets too.
+config.replaceMap.set(/ *\[[^\]]*]/g, '');
+// Replace 'click here' text that is meant for the emails.
+// TODO: Make this capture 'click here\s.*.' Need to capture any generic 'click here to do x'    
+config.replaceMap.set(/(click here)(?:(\s)*to learn more)?/gim, '');
+// Replace text that is meant as a link for the emails.    
+config.replaceMap.set(/^(Sermon Series)$/gim, '');
+// Replace text that is meant as a link for the emails.     
+config.replaceMap.set(/(Listen to Sermons Here.)/gim, '');
+// Replace text that is meant as a link for the emails.     
+config.replaceMap.set(/(Set up an E-Giving account today.)/gim, '');
+// Replace text that is meant for the emails.
+config.replaceMap.set(/Reply to this (email|e-mail)./gim, '');
+config.replaceMap.set(/rsvp and more info here/gim, '');
+// Replace stray character that regex didn't get.
+config.replaceMap.set(/^\]/, '');    
+
 module.exports = config;
